cbd573cb7dd1b51c9a1d57e8575af2a44019a994
David Sparks <davidsparks@gmail.com>
2012-10-09 22:42:47 -0700

    add reentrant functions.  tighten up original functions now that they are not expected to be threadsafe.  check the last issued tuid and emit a warning (and fix it) if its not unique. tuid32 is essentially unixtime, but is there for 32/64 symmetry in the API.

--- tuid.c
+++ tuid.c
@@ -1,10 +1,12 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include <time.h>
+#include "tuid.h"
 
 
 uint64_t tuid64(void)
 {
+    static uint64_t last = 0;
     struct timespec tp;
 
     clock_gettime(CLOCK_REALTIME, &tp);
@@ -15,18 +17,69 @@ uint64_t tuid64(void)
 
     t64 |= tp.tv_nsec;
 
+    if (t64 <= last) {
+        ++last;
+#ifdef WARNINGS
+        fprintf(stderr, "tuid64() collision at time: %" PRIu64 ", forced tuid64 to:  %" PRIu64 "\n", t64, last);
+#endif
+        t64 = last;
+    }
+    else {
+        last = t64;
+    }
+
     return t64;
 }
 
-uint32_t tuid32(void)
+uint64_t tuid64_r(void * ctx)
 {
     struct timespec tp;
 
     clock_gettime(CLOCK_REALTIME, &tp);
 
-    uint32_t t32 = tp.tv_sec & 0xFFFF0000;
+    uint64_t t64 = tp.tv_sec;
 
-    t32 |= tp.tv_nsec & 0xFFFF;
+    t64 <<= 32;
+
+    t64 |= tp.tv_nsec & 0xFF00;
+
+    t64 |= (((s_t64 *)ctx)->id);
+
+    return t64;
+}
+
+uint32_t tuid32(void)
+{
+    static uint32_t last = 0;
+    struct timespec tp;
+
+    clock_gettime(CLOCK_REALTIME, &tp);
+
+    uint32_t t32 = tp.tv_sec & 0xFFFFFFFF;
+
+    if (t32 <= last) {
+        ++last;
+#ifdef WARNINGS
+        fprintf(stderr, "tuid32() collision at time: %d, forced tuid32 to: %d\n", t32, last);
+#endif
+        t32 = last;
+    }
+    else {
+        last = t32;
+    }
+
+    return t32;
+}
+
+uint32_t tuid32_r(void * ctx)
+{
+    struct timespec tp;
+
+    clock_gettime(CLOCK_REALTIME, &tp);
+
+    uint32_t t32 = tp.tv_sec & 0xFFFFFFF0;
+
+    t32 |= (((s_t32 *)ctx)->id);
 
     return t32;
 }
--- tuid.h
+++ tuid.h
@@ -4,4 +4,22 @@
 extern uint64_t tuid64(void);
 extern uint32_t tuid32(void);
 
+typedef struct {
+    int id;
+    int T_bits;
+    int t_bits;
+    int r_bits;
+} s_t64;
+
+typedef struct {
+    int id;
+    int T_bits;
+    int t_bits;
+    int r_bits;
+} s_t32;
+
+
+extern uint64_t tuid64_r(void *);
+extern uint32_t tuid32_r(void *);
+
 #endif
