b9a7e0656e43e6e8e6454d861445b693cebbb8f4
David Sparks <davidsparks@gmail.com>
2013-10-15 00:02:58 -0700

    update README cleanup test suite (that commented section will appear in another test)

--- t/01_smoke64.c
+++ t/01_smoke64.c
@@ -23,33 +23,6 @@ int main(void)
     t = tuid64_r(0);
     ok(t == NULL, "Test invalid context, expected 0 got %" PRIx64, t);
 
-/*
-//    ctx->nsec_min_increment = nsec_min_increment((ctx->nsec_mask << ctx->nsec_shift));
-
-    ctx->id        = 0x0000000012000000ULL;
-
-    ctx->counter = ctx->counter_max = 0xF;
-    ctx->counter_shift = 8;
-
-    ctx->last = 0;
-
-    ctx->random = 0x5248c8561600f46dULL;
-    ctx->random_mask = 0xFF;
-    ctx->random_shift = 4;
-
-    int i;
-    for (i=0; i < 0x02FF; i++) {
-
-        uint64_t tid64 = tuid64_r(ctx);
-        if (tid64 == 0) {
-            printf("FATAL: no tuid\n");
-            exit(1);
-        }
-//        printf("%d Got a tuid64: %" PRIu64 "\n", i, tid64);
-//        printf("Got a tuid64: %" PRIx64 "\n", tid64);
-    }
-*/
-
     tuid64_end(ctx2);
     free(ctx);
     return 0;
