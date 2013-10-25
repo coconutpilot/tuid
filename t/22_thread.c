c73f65dbc631b8d60f7aff1e0cedb062d49737ad
David Sparks <davidsparks@gmail.com>
2013-10-24 22:53:56 -0700

    Threaded test, still needs some work for failing case.

--- t/22_thread.c
+++ t/22_thread.c
@@ -4,17 +4,30 @@
 #include "tuid.h"
 #include "tap/tap.h"
 
-#define THREADS 4
-#define ITERATIONS 10
+#define THREADS 8
+#define ITERATIONS 100000
+
+typedef struct arg_s {
+    int      tid;
+    FILE     *fp;
+    tuid64_s *ctx;
+} arg_s;
 
 void * test_tuid(void * t)
 {
+    arg_s *a = t;
     int i;
     for (i = 0; i < ITERATIONS; i++) {
-        tuid64_s *ctx = tuid64_init(0);
-        uint64_t t64 = tuid64_r(ctx);
-        printf("%" PRIX64 "\n", t64);
+        uint64_t t64 = tuid64_r(a->ctx);
+        fprintf(a->fp, "%" PRIX64 "\n", t64);
     }
+    if (fclose(a->fp)) {
+        BAIL_OUT("Error: fclose()");
+    }
+
+    tuid64_end(a->ctx);
+    a->ctx = 0;
+    free(a);
     pthread_exit(NULL);
 }
 
@@ -22,13 +35,39 @@ int main(void)
 {
     plan(NO_PLAN);
 
+    FILE *cmd;
+
+    /* fork and exec replacement */
+    cmd = popen("mkdir tmp", "r");
+    pclose(cmd);
+
     pthread_t threads[THREADS];
 
     int t;
     for (t = 0; t < THREADS; t++) {
         diag("Starting thread %d\n", t);
+
+        tuid64_s *ctx = tuid64_init(0);
+
+        ctx->nsec_shift = 4;
+        ctx->id = t;
+
+        char fn[1000];
+        FILE *fp;
+        sprintf(fn, "tmp/%d.txt", t);
+
+        fp = fopen(fn, "w");
+        if (! fp) {
+            BAIL_OUT("fopen failed");
+        }
+
+        arg_s *myarg = malloc(sizeof(* myarg));
+        myarg->ctx = ctx;
+        myarg->tid = t;
+        myarg->fp = fp;
+
         int rc;
-        rc = pthread_create(&threads[t], NULL, test_tuid, (void *)t);
+        rc = pthread_create(&threads[t], NULL, test_tuid, (void *)myarg);
         if (rc) {
             fail("pthread_create failed: %d\n", rc);
             return 13;
@@ -45,5 +84,30 @@ int main(void)
         }
         pass("thread exited with status: %d\n", (int)t_rc);
     }
-    return 0;
+
+    cmd = popen("cd tmp; sort * | uniq -d" , "r");
+    if (! cmd) {
+        BAIL_OUT("Reading results failed");
+    }
+
+    char buf[1024];
+
+    ssize_t r;
+    int failed = 0;
+
+    while (r = fread(buf, sizeof(buf), 1, cmd)) {
+        /* XXX: this can print out garbage as buf may not be NULL terminated */
+        diag(buf);
+        ++failed;
+    }
+    pclose(cmd);
+
+    if (failed) {
+        fail("Duplicate tuid found");
+    }
+    else {
+        pass();
+    }
+
+    return exit_status();
 }
